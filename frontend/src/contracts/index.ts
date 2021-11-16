633fabd2aab8bb2cffad96d392599c48f7cdcfce
TalentDev1128 <razor@outsource.codes>
2021-11-16 03:26:33 -0800

    Put live contract address and abi

--- frontend/src/contracts/index.ts
+++ frontend/src/contracts/index.ts
@@ -1,6 +1,6 @@
 export const elvesContractAddress =
-  "0xCAccb157236B0969fe21eb486f2Bc5dc0662a5c5";
+  "0x72541ad75e05bc77c7a92304225937a8f6653372";
 export const blootContractAddress =
   "0x45c3844Dea2e9Fe9226524411DE6d907188A1a9F";
 export const derivativeContractAddress =
-  "0x5E901eed5AF868a7eB5c2c28840c7861099c026d";
+  "0xC84A65BeeD790febE15bA079f2eD8E7Aeefa4656";
